b998ddd549e839992c1abecf20cee49d98a29203
GLDNRMZ <122560170+GLDNRMZ@users.noreply.github.com>
2023-09-02 16:37:07 -0400

    Update config.lua
    Add TP Time

--- lb-aidoc/config.lua
+++ lb-aidoc/config.lua
@@ -1,9 +1,11 @@
--------------------
--- CONFIG --
--------------------
+--------------------
+    -- CONFIG --
+--------------------
 Config = {}
 
-Config.Doctor = 0 -- Minimum Amount of EMS to work
-Config.Price = 1000
-Config.ReviveTime = 20000  --in msec
+Config.Debug = false
 
+Config.Doctor = 1           -- Minimum Amount of EMS to work
+Config.Price = 1500         -- Cost for AI Doc Services
+Config.ReviveTime = 30      -- in seconds
+Config.TPTime = 60          -- in seconds
