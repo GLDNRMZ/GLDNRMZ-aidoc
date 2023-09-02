0b4ebc30a14b9a5a5ef1b31d9fd9932ed397e5c4
GLDNRMZ <122560170+GLDNRMZ@users.noreply.github.com>
2023-09-02 16:36:31 -0400

    Update client.lua
    TP rework

--- lb-aidoc/client.lua
+++ lb-aidoc/client.lua
@@ -1,61 +1,51 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local Active = false
+local Revived = false
+local calledHelp = false
 local veh = nil
 local ped1 = nil
 local ped2 = nil
-local spam = true
 local ANIM_DICT = "mini@cpr@char_a@cpr_str"
-local REVIVE_TIME = Config.ReviveTime
+local REVIVE_TIME = Config.ReviveTime * 1000
 local PRICE = Config.Price
 
-function Notify(msg, state)
-    QBCore.Functions.Notify(msg, state)
-end
-
 local lastDoctorTime = 0
+local sleep = 200
 
-RegisterCommand("localdoctor", function(source, args, raw)
+TriggerEvent('chat:addSuggestion', '/help', 'Call a Local Paramedic')
+RegisterCommand("help", function(source, args, raw)
     local playerData = QBCore.Functions.GetPlayerData()
 
     if not playerData.metadata["isdead"] and not playerData.metadata["inlaststand"] then
-        Notify("This can only be used when dead", "error")
+        QBCore.Functions.Notify("This can only be used when dead", "error")
+		TriggerServerEvent("qb-log:server:CreateLog", "commands", "Help Command Fail", "red", "Player **".. playerData.name .. "** Character Name: **" .. playerData.charinfo["firstname"] .. " " .. playerData.charinfo["lastname"] .. "** attempted to use /help while not dead.")
         return
     end
 
-    if not spam then
+    if not playerData.metadata["injail"] then
+        QBCore.Functions.Notify("This can't be used when in jail", "error")
         return
     end
 
     QBCore.Functions.TriggerCallback('lb:docOnline', function(EMSOnline, hasEnoughMoney)
         if EMSOnline <= Config.Doctor and hasEnoughMoney then
-            SpawnVehicle(GetEntityCoords(PlayerPedId()))
-            TriggerServerEvent('lb:charge')
-            Notify("Medic is arriving")
-            lastDoctorTime = GetGameTimer()
+            if not calledHelp then
+                calledHelp = true
+                Revived = false
+                lastDoctorTime = GetGameTimer()
+                QBCore.Functions.Notify("Paramedic is on its Way", "primary", 60000)
+                TriggerServerEvent("qb-log:server:CreateLog", "commands", "Help Command Success", "blue", "Player **".. playerData.name .. "** Character Name: **" .. playerData.charinfo["firstname"] .. " " .. playerData.charinfo["lastname"] .. "** used /help! A ambulance is on the way to revive them.")
+                SpawnVehicle(GetEntityCoords(PlayerPedId()))
+            else
+                QBCore.Functions.Notify("You've already called for help, please be patient", "error")
+            end
         elseif EMSOnline > Config.Doctor then
-            Notify("There are too many medics on duty", "error")
+            QBCore.Functions.Notify("There are too many medics on duty", "error")
         elseif not hasEnoughMoney then
-            Notify("Not Enough Money", "error")
-        else
-            Notify("Wait Paramedic is on its Way", "primary")
+            QBCore.Functions.Notify("Not Enough Money", "error")
         end
     end)
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000)
-
-        if lastDoctorTime > 0 and GetGameTimer() - lastDoctorTime >= 60000 then
-            local playerPed = PlayerPedId()
-            local ld = GetEntityCoords(ped1)
-            if DoesEntityExist(ped1) then
-                SetEntityCoords(playerPed, ld.x + 5.0, ld.y + 5.0, ld.z, 0, 0, 0, 1)
-            end
-            lastDoctorTime = 0
-        end
-    end
 end)
 
 function SpawnVehicle(x, y, z)
@@ -65,10 +55,10 @@ function SpawnVehicle(x, y, z)
     RequestModel(vehhash)
     RequestModel('s_m_m_paramedic_01')
     while not HasModelLoaded(vehhash) or not HasModelLoaded('s_m_m_paramedic_01') do
-        Citizen.Wait(1)
+        Wait(1)
     end
 
-    local spawnRadius = 40
+    local spawnRadius = 60
     local loc = GetEntityCoords(PlayerPedId())
     local found, spawnPos, spawnHeading = GetClosestVehicleNodeWithHeading(loc.x + math.random(-spawnRadius, spawnRadius), loc.y + math.random(-spawnRadius, spawnRadius), loc.z, 0, 3, 0)
 
@@ -89,6 +79,9 @@ function SpawnVehicle(x, y, z)
     SetBlipFlashes(docBlip, true)
     SetBlipColour(docBlip, 5)
 
+    SetEntityInvincible(docPed, true)
+    SetEntityInvincible(passengerPed, true)
+
     PlaySoundFrontend(-1, "Text_Arrive_Tone", "Phone_SoundSet_Default", 1)
     Wait(2000)
 
@@ -99,43 +92,56 @@ function SpawnVehicle(x, y, z)
     ped1 = docPed
 	ped2 = passengerPed
     Active = true
+    docEnRoute()
 end
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(200)
-        if Active then
-            local playerPed = GetPlayerPed(-1)
-            local loc = GetEntityCoords(playerPed)
-            local lc = GetEntityCoords(veh)
-            local ld = GetEntityCoords(ped1)
-            local dist = Vdist(loc, lc)
-            local dist1 = Vdist(loc, ld)
-            if dist <= 10 then
-                if Active then
-                    TaskGoToCoordAnyMeans(ped1, loc.x, loc.y, loc.z, 3.0, 0, 0, 786603, 0xbf800000)
-                end
-                if dist1 <= 1.33 then 
-                    Active = false
-                    ClearPedTasksImmediately(ped1)
-                    if IsPedInAnyVehicle(playerPed, false) then
-                        local veh = GetVehiclePedIsIn(playerPed, false)
-                        SetEntityCoords(playerPed, ld.x + 1.0, ld.y + 1.0, ld.z, 0, 0, 0, 1)
-                        DoctorNPC()
-                    else
-                        DoctorNPC()
-                    end
-                    
-                end
+function docEnRoute()
+    while not Revived do
+        sleep = 200
+        local playerPed = GetPlayerPed(-1)
+        local loc = GetEntityCoords(playerPed)
+        local lc = GetEntityCoords(veh)
+        local ld = GetEntityCoords(ped1)
+        local dist = #(loc - lc)
+        local dist1 = #(loc - ld)
+        local time = GetGameTimer()
+        local timer = (time - lastDoctorTime)/1000
+        if Config.Debug then print('Doc En Route - Dist: '..dist, 'Dist1: '..dist1, 'Time: '..time, 'lastDoctorTime: '..lastDoctorTime, 'Timer: '..timer..' seconds') end
+        if dist <= 10 then
+            SetVehicleSiren(docVeh, false)
+            if IsPedInAnyVehicle(playerPed, false) then
+                local veh = GetVehiclePedIsIn(playerPed, false)
+                TaskLeaveVehicle(playerPed, veh, 16)
+            elseif Active then
+                TaskGoToCoordAnyMeans(ped1, loc.x, loc.y, loc.z, 3.0, 0, 0, 786603, 0xbf800000)
+            end
+            if dist1 <= 1.5 then
+                Active = false
+                DoctorNPC()
+                break
+            end
+        elseif lastDoctorTime > 0 and timer >= Config.TPTime then
+            sleep = 3000
+            SetVehicleSiren(docVeh, false)
+            ClearPedTasksImmediately(ped1)
+            if Config.Debug then print('TP to AI Doc Triggered') end
+            if DoesEntityExist(ped1) then
+                DoScreenFadeOut(500)
+                SetEntityCoords(playerPed, ld.x + 5.0, ld.y + 5.0, ld.z, 0, 0, 0, 1)
+                Wait(2500)
+                DoScreenFadeIn(1500)
+                lastDoctorTime = 0
             end
         end
+        Wait(sleep)
     end
-end)
+end
 
 function DoctorNPC()
+    if Config.Debug then print('DoctorNPC Triggered') end
 	RequestAnimDict(ANIM_DICT)
 	while not HasAnimDictLoaded(ANIM_DICT) do
-		Citizen.Wait(1000)
+		Wait(1000)
 	end
 
 	TaskPlayAnim(ped1, ANIM_DICT, "cpr_pumpchest", 1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
@@ -151,18 +157,29 @@ function DoctorNPC()
 		disableCombat = true,
 	}, {}, {}, {}, function()
 		ClearPedTasks(ped1)
-		Citizen.Wait(500)
+		Wait(500)
 		TriggerEvent("hospital:client:Revive")
+        TriggerServerEvent('lb:charge')
 		StopScreenEffect('DeathFailOut')
-		Notify("Here's a lollipop, you were charged: "..PRICE, "success")
+		QBCore.Functions.Notify("Your treatment is done, you were charged: $"..Config.Price, "success")
+		TriggerServerEvent("qb-log:server:CreateLog", "commands", "Help Command Healed", "green", "Player **".. QBCore.Functions.GetPlayerData().name .. "** Character Name: **" .. QBCore.Functions.GetPlayerData().charinfo["firstname"] .. " " .. QBCore.Functions.GetPlayerData().charinfo["lastname"] .. "** was healed by the AI Doc.")
+        calledHelp = false
+        Revived = true
 
-		RemovePedElegantly(ped1)
+		--RemovePedElegantly(ped1)
 		TaskEnterVehicle(ped1, veh, 0, 2, 3.0, 1, 0)
 		TaskVehicleDriveWander(ped1, veh, 25.0, 524295)
 		Wait(20000)
 		DeleteEntity(veh)
 		DeleteEntity(ped1)
 		DeleteEntity(ped2)
-		spam = true
 	end)
 end
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		DeleteEntity(veh)
+		DeleteEntity(ped1)
+		DeleteEntity(ped2)
+    end
+end)
